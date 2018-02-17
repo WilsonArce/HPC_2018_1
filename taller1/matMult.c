bf470dd207236bff94f40822fbf64627a96ab81a
WilsonArce <wdarcedev@gmail.com>
2018-02-17 00:52:02 -0500

    mat mult

---
+++ taller1/matMult.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[]){
+  FILE *f1, *f2, *f3;
+  f1 = fopen("mat1.csv","w");
+  f2 = fopen("mat2.csv","w");
+  f3 = fopen("ansMat.csv","w");
+
+  float *mat1, *mat2, *ansMat;
+
+  int m1Row = atoi(argv[1]);
+  int m1Col = atoi(argv[2]);
+  int m2Row = atoi(argv[3]);
+  int m2Col = atoi(argv[4]);
+
+  if (m1Col == m2Row){
+    float	**m1;
+    m1 = (float **)malloc(m1Row * sizeof(float *));
+
+    float	**m2;
+    m2 = (float **)malloc(m2Row * sizeof(float *));
+
+    time_t t;
+    srand((unsigned) time(&t));
+
+    for(int i=0; i < m1Row; i++){
+      m1[i] = (float *)malloc(m1Col * sizeof(float));
+      for(int j=0; j < m1Col; j++){
+        float num1 = (float)rand()/(float)(RAND_MAX)*100;
+        m1[i][j] = num1;
+        fprintf(f1, "%.3f,", m1[i][j]);
+      }
+      fseek(f1, -1, SEEK_END);
+  	  fprintf(f1, "\n");
+    }
+
+    for(int i=0; i < m2Row; i++){
+      m2[i] = (float *)malloc(m2Col * sizeof(float));
+      for(int j=0; j < m2Col; j++){
+        float num2 = (float)rand()/(float)(RAND_MAX)*100;
+        m2[i][j] = num2;
+        fprintf(f2, "%.3f,", m2[i][j]);
+      }
+      fseek(f2, -1, SEEK_END);
+  	  fprintf(f2, "\n");
+    }
+
+    float a, b;
+
+    for (int i = 0; i < m1Row; i++) {
+	    for (int j = 0; j < m2Col; j++) {
+	   		float sum = 0;
+	      for (int k = 0; k < m1Col; k++) {
+	        a = m1[i][k];
+	        b = m2[k][j];
+	        sum += a * b;
+	      }
+	      fprintf(f3, "%.3f,", sum);
+	  	}
+	  	fseek(f3, -1, SEEK_END);
+	  	fprintf(f3, "\n");
+	 	}
+
+  }
+
+  return 0;
+}
--- taller1/vecAdd.c
+++ taller1/vecAdd.c
@@ -3,42 +3,42 @@
 #include <time.h>
 
 int main(int argc, char const *argv[]){
-    FILE *f1, *f2, *f3;
-    f1 = fopen("vec1.csv","w");
-    f2 = fopen("vec2.csv","w");
-    f3 = fopen("ansVec.csv","w");
+	FILE *f1, *f2, *f3;
+	f1 = fopen("vec1.csv","w");
+	f2 = fopen("vec2.csv","w");
+	f3 = fopen("ansVec.csv","w");
 
-    float *vec1, *vec2, *ans;
+	float *vec1, *vec2, *ans;
 
-    int size = atoi(argv[1]);
+	int size = atoi(argv[1]);
 
-    //reserva de memoria
-    vec1 = (float *)malloc(size * sizeof(float));
-    vec2 = (float *)malloc(size * sizeof(float));
-    ans = (float *)malloc(size * sizeof(float));
+	//reserva de memoria
+	vec1 = (float *)malloc(size * sizeof(float));
+	vec2 = (float *)malloc(size * sizeof(float));
+	ans = (float *)malloc(size * sizeof(float));
 
-    time_t t;
+	time_t t;
 
-    srand((unsigned) time(&t));
+	srand((unsigned) time(&t));
 
-    //Generacion de archivos
-    for(int i = 0; i < size; i++){
-        float num1 = (float)rand()/(float)(RAND_MAX)*100;
-        vec1[i] = num1;
-        fprintf(f1, "%.3f,", vec1[i]);
+	//Generacion de archivos
+	for(int i = 0; i < size; i++){
+		float num1 = (float)rand()/(float)(RAND_MAX)*100;
+		vec1[i] = num1;
+		fprintf(f1, "%.3f,", vec1[i]);
 
-        float num2 = (float)rand()/(float)(RAND_MAX)*100;
-        vec2[i] = num2;
-        fprintf(f2, "%.3f,", vec2[i]);
+		float num2 = (float)rand()/(float)(RAND_MAX)*100;
+		vec2[i] = num2;
+		fprintf(f2, "%.3f,", vec2[i]);
 
-        ans[i] = vec1[i] + vec2[i];
-        fprintf(f3, "%.3f,", ans[i]);
+		ans[i] = vec1[i] + vec2[i];
+			fprintf(f3, "%.3f,", ans[i]);
 
-    }
-        fseek(f1, -1, SEEK_END); fprintf(f1, "\n");
-        fseek(f2, -1, SEEK_END); fprintf(f2, "\n");
-        fseek(f3, -1, SEEK_END); fprintf(f3, "\n");
+	}
+	fseek(f1, -1, SEEK_END); fprintf(f1, "\n");
+	fseek(f2, -1, SEEK_END); fprintf(f2, "\n");
+	fseek(f3, -1, SEEK_END); fprintf(f3, "\n");
 
-    return 0;
+	return 0;
 
 }
