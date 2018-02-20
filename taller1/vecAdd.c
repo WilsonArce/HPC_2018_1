4c1c2e73a196bc688a32a212712dead7ea281133
WilsonArce <wdarcedev@gmail.com>
2018-02-19 21:37:33 -0500

    vectoradd

--- taller1/vecAdd.c
+++ taller1/vecAdd.c
@@ -10,34 +10,40 @@ int main(int argc, char const *argv[]){
 
 	float *vec1, *vec2, *ans;
 
-	int size = atoi(argv[1]);
+	if (argc == 2){
 
-	//reserva de memoria
-	vec1 = (float *)malloc(size * sizeof(float));
-	vec2 = (float *)malloc(size * sizeof(float));
-	ans = (float *)malloc(size * sizeof(float));
+		int size = atoi(argv[1]);
 
-	time_t t;
+		//reserva de memoria
+		vec1 = (float *)malloc(size * sizeof(float));
+		vec2 = (float *)malloc(size * sizeof(float));
+		ans = (float *)malloc(size * sizeof(float));
 
-	srand((unsigned) time(&t));
+		time_t t;
 
-	//Generacion de archivos
-	for(int i = 0; i < size; i++){
-		float num1 = (float)rand()/(float)(RAND_MAX)*100;
-		vec1[i] = num1;
-		fprintf(f1, "%.3f,", vec1[i]);
+		srand((unsigned) time(&t));
 
-		float num2 = (float)rand()/(float)(RAND_MAX)*100;
-		vec2[i] = num2;
-		fprintf(f2, "%.3f,", vec2[i]);
+		//Generacion de archivos
+		for(int i = 0; i < size; i++){
+			float num1 = (float)rand()/(float)(RAND_MAX)*100;
+			vec1[i] = num1;
+			fprintf(f1, "%.3f,", vec1[i]);
 
-		ans[i] = vec1[i] + vec2[i];
-			fprintf(f3, "%.3f,", ans[i]);
+			float num2 = (float)rand()/(float)(RAND_MAX)*100;
+			vec2[i] = num2;
+			fprintf(f2, "%.3f,", vec2[i]);
 
-	}
-	fseek(f1, -1, SEEK_END); fprintf(f1, "\n");
-	fseek(f2, -1, SEEK_END); fprintf(f2, "\n");
-	fseek(f3, -1, SEEK_END); fprintf(f3, "\n");
+			ans[i] = vec1[i] + vec2[i];
+				fprintf(f3, "%.3f,", ans[i]);
+
+		}
+		fseek(f1, -1, SEEK_END); fprintf(f1, "\n");
+		fseek(f2, -1, SEEK_END); fprintf(f2, "\n");
+		fseek(f3, -1, SEEK_END); fprintf(f3, "\n");
+
+		free(vec1); free(vec2); free(ans);
+
+	}else{printf("Verifique la cantidad de parametros!!\n");}
 
 	return 0;
 
