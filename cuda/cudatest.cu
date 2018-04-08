1ba06a0c9bdfe3ee75171ea9cf871cf463e97674
WilsonArce <wdarcedev@gmail.com>
2018-04-08 12:11:03 -0500

    cuda matmult test

--- cuda/cudatest.cu
+++ cuda/cudatest.cu
@@ -18,10 +18,10 @@ int main(int argc, char const *argv[]){
 
 	int N = 0;
 
-	if(argc == 1){
-		N = atoi(argv[1]);
-	}else{
+	if(argc != 1){
 		printf("Please give a size!!");
+	}else{
+		N = atoi(argv[1]);
 	}
 
 	double timeGPU;
