790a0d460d64d634b4f2c139b0ccd80f596f4a27
WilsonArce <wdarcedev@gmail.com>
2018-04-08 12:18:45 -0500

    cuda matmult test

--- cuda/cudatest.cu
+++ cuda/cudatest.cu
@@ -19,12 +19,11 @@ int main(int argc, char** argv ){
 	int N = 0;
 
 	if(argc != 2){
-		printf("Please give a size!!");
-		return -1;
+		N = 4;
+	}else{
+		N = atoi(argv[1]);
 	}
-
-	N = atoi(argv[1]);
-
+	
 	double timeGPU;
 
 	size_t bytes = N * N * sizeof(int);
