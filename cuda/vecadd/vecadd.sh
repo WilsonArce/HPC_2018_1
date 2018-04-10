c5447b3af34e7774de476e9bbfda77b158048e9f
WilsonArce <wdarcedev@gmail.com>
2018-04-09 21:20:14 -0500

    cuda vecadd

---
+++ cuda/vecadd/vecadd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=cudaVecadd
+#SBATCH --output=ans_cudaVecadd
+#SBATCH --ntasks=3
+#SBATCH --nodes=3
+#SBATCH --gres=gpu:1
+
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+export CUDA_VISIBLE_DEVICES=0
+
+./vecadd $1
