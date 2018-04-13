401681cdfff7f837a207b6d76b7d51e5cd0d3c85
WilsonArce <wdarcedev@gmail.com>
2018-04-13 12:21:54 -0500

    cuda matmult

--- parcial2_p1/matmult.cu
+++ parcial2_p1/matmult.cu
@@ -111,7 +111,7 @@ int main(int argc, char const *argv[])
     free(h_m1); free(h_m2); free(h_ans);
 	  cudaFree(d_m1); cudaFree(d_m2); cudaFree(h_ans);
 
-    printf("ans[2] = %d\n",h_ans[2]);
+    //printf("ans[2] = %d\n",h_ans[2]);
 
   }
   return 0;
