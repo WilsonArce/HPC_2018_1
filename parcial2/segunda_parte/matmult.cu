c204f50f4de495bb9740c59afaf35228f95bfee9
WilsonArce <wdarcedev@gmail.com>
2018-05-03 20:23:12 -0500

    secuential matmult

--- parcial2/segunda_parte/matmult.cu
+++ parcial2/segunda_parte/matmult.cu
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#define N 100
+#define N 100
 
-void sec_matMult(int A, int aCol, int aRow, int B, int bCol, int bRow, int *C){
+void sec_matMult(int* A, int aCol, int aRow, int* B, int bCol, int bRow, int* C){
 	for (int i = 0; i < aRow; i++) {
     for (int j = 0; j < bCol; j++) {
    		int sum = 0;
