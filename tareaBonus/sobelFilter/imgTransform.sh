ec2d06803e91fe5d6ac6823cb7de49a7d29b9463
WilsonArce <utp@utp>
2018-05-04 15:08:57 -0500

    sobel filter

--- tareaBonus/sobelFilter/imgTransform.sh
+++ tareaBonus/sobelFilter/imgTransform.sh
@@ -12,6 +12,7 @@ export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRAR
 export CUDA_VISIBLE_DEVICES=0
 
 #./sobel_GCSmem images/512x512.jpg
+make
 
 FILES=../images/*
 n=20
