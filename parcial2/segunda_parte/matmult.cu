17bcf2a7e67e76dcecebf314190de21ad6ccd8af
WilsonArce <wdarcedev@gmail.com>
2018-05-06 18:08:07 -0500

    matmul shared-mem

--- parcial2/segunda_parte/matmult.cu
+++ parcial2/segunda_parte/matmult.cu
@@ -104,7 +104,7 @@ int main(int argc, char** argv ){
     //Asignacion de memoria en el Device
     printf("> Asignacion de memoria en el Device...\n");
     err = cudaGetErrorString(cudaMalloc((void **) &d_m1, m1Size));
-    if(err == "no error") printf(" -cudaMalloc d_m1: %s\n",err);
+    if(err != cudaSuccess) printf(" -cudaMalloc d_m1: %s\n",err);
     err = cudaGetErrorString(cudaMalloc((void **) &d_m2, m1Size));
     if("no error" != err) printf(" -cudaMalloc d_m2: %s\n",err);
     // printf(" -cudaMalloc d_m1: %s\n",cudaGetErrorString(cudaMalloc((void **) &d_m1, m1Size)));
