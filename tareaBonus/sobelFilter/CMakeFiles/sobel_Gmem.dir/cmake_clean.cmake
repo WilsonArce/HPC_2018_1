92ecccafd4412d2249abb816a407086e0856988d
Wilson Arce <wdarcedev@gmail.com>
2018-05-07 20:00:28 -0500

    GPU info

---
+++ tareaBonus/sobelFilter/CMakeFiles/sobel_Gmem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sobel_Gmem.dir/sobel_Gmem_generated_sobel_Gmem.cu.o"
+  "sobel_Gmem.pdb"
+  "sobel_Gmem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sobel_Gmem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
