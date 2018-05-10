588019f93281020d26d13cc02450ece7eb8d5685
WilsonArce <wdarcedev@gmail.com>
2018-05-09 20:38:36 -0500

    shared matmult

--- parcial2/segunda_parte/matmult.cu
+++ parcial2/segunda_parte/matmult.cu
@@ -5,14 +5,14 @@
 #define tile 32
 
 //Multiplicacion secuencial
-void sec_matMult(int* A, int aCol, int aRow, int* B, int bCol, int bRow, int* C){
-	for (int i = 0; i < aRow; i++) {
-    for (int j = 0; j < bCol; j++) {
+void sec_matMult(int* A, int* B, int* C, int size){
+	for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
    		int sum = 0;
-      for (int k = 0; k < aCol; k++) {
-        sum += A[j * aCol + k] * B[k * aCol + i];
+      for (int k = 0; k < size; k++) {
+        sum += A[j * size + k] * B[k * size + i];
       }
-   	  C[j * aCol + i] = sum;
+   	  C[j * size + i] = sum;
   	}
  	}
 }
@@ -59,69 +59,56 @@ __global__ void sdmem_matMult(int* m1, int* m2, int* ansS, int n){
   ansS[row * n + col] = sum;
 }
 
+void showAns(const char* type, int n, int* ans){
+  printf("%d x %d\n",n,n);
+  for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			printf("%d",ans[i * n + j]);
+      j < n ? printf(",") : printf("\n");
+		}
+	}
+}
+
 
 int main(int argc, char** argv ){
 
   //Definicion de variables
-  FILE *f1, *f2, *f3, *f4, *f5;
   double secTime, globalTime, sharedTime;
-  int *h_m1, *h_m2, *h_ans, *h_ansG, *h_ansS;
-  int *d_m1, *d_m2, *d_ansG, *d_ansS;
-  int m1Row, m1Col, m2Row, m2Col; 
+  int *h_m1, *h_m2, *h_ans;
+  int *d_m1, *d_m2, *d_ans;
+  int matSize; 
 
   cudaError_t err = cudaSuccess;
 
   if (argc != 2){
     printf("Cantidad de parametros incorrecta!!\n");
   }else{
-    //Creación de archivos
-    matGen(atoi(argv[1]));
-    //f1 = fopen(argv[1],"r");
-    //f2 = fopen(argv[2],"r");
-    f1 = fopen("mat1.txt","r");
-    f2 = fopen("mat2.txt","r");
-    f3 = fopen("sec_ans.txt","w");
-    f4 = fopen("glo_ans.txt","w");
-    f5 = fopen("sha_ans.txt","w");
-    //Lectura de dimensiones de las matrices
-    fscanf(f1, "%d", &m1Row); fscanf(f1, "%d", &m1Col);
-    fscanf(f2, "%d", &m2Row); fscanf(f2, "%d", &m2Col);
+    //Tamaño de las matrices
+    matSize = atoi(argv[1]));
 
     //Definición de tamaño para asignar memoria
-    size_t m1Size = m1Row * m1Col * sizeof(int);
-    size_t m2Size = m2Row * m2Col * sizeof(int);
-    size_t ansSize = m1Col * m2Row * sizeof(int);
+    size_t dataSize = matSize * matSize * sizeof(int);
 
     //Asignación de memoria en el Host
-    h_m1 = (int *)malloc(m1Size);
-    h_m2 = (int *)malloc(m2Size);
-    h_ans = (int *)malloc(ansSize);
-    h_ansG = (int *)malloc(ansSize);
-    h_ansS = (int *)malloc(ansSize);
-
-    //Lectura de archivos y almacenamiento en el Host
-    readAllocFile(f1, h_m1, m1Row, m1Col);
-    readAllocFile(f2, h_m2, m2Row, m2Col);
-
-    printf("m1 size: %lu",sizeof(h_m1));
+    h_m1 = (int *)malloc(dataSize);
+    h_m2 = (int *)malloc(dataSize);
+    h_ans = (int *)malloc(dataSize);
 
     //Asignacion de memoria en el Device
     printf("> Asignacion de memoria en el Device...");
-    err = cudaMalloc((void **) &d_m1, m1Size);
+    err = cudaMalloc((void **) &d_m1, dataSize);
     if(err != cudaSuccess){ printf(" -cudaMalloc d_m1: %s\n",cudaGetErrorString(err)); return 0;}
-    err = cudaMalloc((void **) &d_m2, m2Size);
+    err = cudaMalloc((void **) &d_m2, dataSize);
     if(err != cudaSuccess){ printf(" -cudaMalloc d_m2: %s\n",cudaGetErrorString(err)); return 0;}
-    err = cudaMalloc((void **) &d_ansG, ansSize);
-    if(err != cudaSuccess){ printf(" -cudaMalloc d_ansG: %s\n",cudaGetErrorString(err)); return 0;}
-    err = cudaMalloc((void **) &d_ansS, ansSize);
-    if(err != cudaSuccess){ printf(" -cudaMalloc d_ansS: %s\n",cudaGetErrorString(err)); return 0;}
+    err = cudaMalloc((void **) &d_ans, dataSize);
+    if(err != cudaSuccess){ printf(" -cudaMalloc d_ans: %s\n",cudaGetErrorString(err)); return 0;}
     printf("ok!!!\n");
 
     //Copia de datos del Host al Device
     printf("> Copia de datos H -> D...");
-    err = cudaMemcpy(d_m1, h_m1, m1Size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_m1, h_m1, dataSize, cudaMemcpyHostToDevice);
     if(err != cudaSuccess){ printf(" -cudaMemcpy h_m1 -> d_m1: %s\n",cudaGetErrorString(err)); return 0;}
-    err = cudaMemcpy(d_m2, h_m2, m1Size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_m2, h_m2, dataSize, cudaMemcpyHostToDevice);
     if(err != cudaSuccess){ printf(" -cudaMemcpy h_m2 -> d_m2: %s\n",cudaGetErrorString(err)); return 0;}
     printf("ok!!!\n");
 
@@ -129,56 +116,54 @@ int main(int argc, char** argv ){
 
     //Llamado a la multiplicacion secuencial
     clock_t startSecTime = clock();
-    sec_matMult(h_m1, m1Col, m1Row, h_m2, m2Col, m2Row, h_ans);
+    sec_matMult(h_m1, h_m2, h_ans, matSize);
     secTime = ((double)(clock()-startSecTime))/CLOCKS_PER_SEC;
     printf("> Secuencial = %.6fs\n",secTime);
 
-    //Generacion de archivo respuesta
-    if(m1Row <= 4) setAnsFile("secuencial", m1Row, m2Col, h_ans, f3);
+    //Imprime respuesta
+    if(matSize <= 4) showAns("secuencial", size, h_ans);
 
     /////////////////////////////////////
 
-    int threads = m1Row;//Cantidad de hilos
+    int matSize;//Cantidad de hilos
     //Definicion de estructuras para cantidad de Hilos y Bloques
     dim3 blockDim(tile,tile);
-	  dim3 gridDim(ceil((float)threads/32.0), ceil((float)threads/32.0));
+	  dim3 gridDim(ceil((float)matSize/blockDim.x), ceil((float)matSize/blockDim.y));
 
     //Multiplicacion paralela con memoria global
     clock_t startGlobalTime = clock();
-    gbmem_matMult<<<gridDim, blockDim>>>(d_m1, d_m2, d_ansG, threads);
+    gbmem_matMult<<<gridDim, blockDim>>>(d_m1, d_m2, d_ans, matSize);
     err = cudaDeviceSynchronize();
     if(err != cudaSuccess){ printf(" -Kernel call (global-mem): %s\n",cudaGetErrorString(err)); return 0;}
 
     //Copia de datos del Device al Host
-    err = cudaMemcpy(h_ansG, d_ansG, ansSize, cudaMemcpyDeviceToHost);
-    if(err != cudaSuccess){ printf(" -cudaMemcpy d_ansG -> h_ansG: %s\n",cudaGetErrorString(err)); return 0;}
+    err = cudaMemcpy(h_ans, d_ans, dataSize, cudaMemcpyDeviceToHost);
+    if(err != cudaSuccess){ printf(" -(gMem) cudaMemcpy d_ans -> h_ans: %s\n",cudaGetErrorString(err)); return 0;}
     globalTime = ((double)(clock()-startGlobalTime))/CLOCKS_PER_SEC;
     printf("> Memoria global (cuda) = %.6fs\n",globalTime);
     cudaDeviceSynchronize();
 
-    if(m1Row <= 4) setAnsFile("global-mem", m1Row, m2Col, h_ansG, f4);
+    if(matSize <= 4) showAns("global-mem", size, h_ans);
 
     ///////////////////////////////////////
 
     //Multiplicacion paralela con memoria compartida
     clock_t startSharedTime = clock();
-    sdmem_matMult<<<gridDim, blockDim>>>(d_m1, d_m2, d_ansS, threads);
+    sdmem_matMult<<<gridDim, blockDim>>>(d_m1, d_m2, d_ans, matSize);
     err = cudaDeviceSynchronize();
     if(err != cudaSuccess){ printf(" -Kernel call (shared-mem): %s\n",cudaGetErrorString(err)); return 0;}
     
     //Copia de datos del Device al Host
     err = cudaMemcpy(h_ansS, d_ansS, ansSize, cudaMemcpyDeviceToHost);
-    if(err != cudaSuccess){ printf(" -cudaMemcpy d_ansS -> h_ansS: %s\n",cudaGetErrorString(err)); return 0;}
+    if(err != cudaSuccess){ printf(" -(sMem) cudaMemcpy d_ansS -> h_ans: %s\n",cudaGetErrorString(err)); return 0;}
     sharedTime = ((double)(clock()-startSharedTime))/CLOCKS_PER_SEC;
     printf("> Memoria compartida (cuda) = %.6fs\n",sharedTime);
 
-    if(m1Row <= 4) setAnsFile("shared-mem", m1Row, m2Col, h_ansS, f5);
-
-    fclose(f1); fclose(f2); fclose(f3); fclose(f4); fclose(f5);
+    if(matSize <= 4) showAns("shared-mem", size, h_ans);
 
     //Liberacion de memoria
-    free(h_m1); free(h_m2); free(h_ans); free(h_ansG); free(h_ansS);
-	  cudaFree(d_m1); cudaFree(d_m2); cudaFree(d_ansG); cudaFree(d_ansS);
+    free(h_m1); free(h_m2); free(h_ans);
+	  cudaFree(d_m1); cudaFree(d_m2); cudaFree(d_ans);
 
   }
 
