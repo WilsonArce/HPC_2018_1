94dcc2c3359267db4ea6af50949fd1d3a2906426
WilsonArce <wdarcedev@gmail.com>
2018-05-10 20:49:26 -0500

    shared matmult

--- parcial2/segunda_parte/matmult.sh
+++ parcial2/segunda_parte/matmult.sh
@@ -27,7 +27,7 @@ do
   do
     OUT=($(./matmult $i))
     ANS[0]=${OUT[0]}
-    ${$ANS[1]}+=${OUT[1]}
+    "${ANS[1]}+=${OUT[1]}"
     ANS[2]+=${OUT[2]}
     ANS[3]+=${OUT[3]}
     ANS[4]+=${OUT[4]}
