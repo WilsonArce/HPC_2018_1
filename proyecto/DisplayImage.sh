3561d56e832e198778b8ede6abc3442b6d767a9e
WilsonArce <wdarcedev@gmail.com>
2018-06-04 12:32:21 -0500

    cuda image to bin test

---
+++ proyecto/DisplayImage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=DisplayImage
+#SBATCH --output=outDisplayImage.txt
+#SBATCH --ntasks=2
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:1
+
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+export CUDA_VISIBLE_DEVICES=0
+
+cmake .
+make
+./DisplayImage secret.jpg cover.jpg
