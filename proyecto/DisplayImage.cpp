2a6a19669331d4898914aeebe835c3e9c0a4979c
WilsonArce <wdarcedev@gmail.com>
2018-06-04 09:05:35 -0500

    hidding secret image

--- proyecto/DisplayImage.cpp
+++ proyecto/DisplayImage.cpp
@@ -36,28 +36,58 @@ void imgToBin(unsigned char *imgDec, unsigned char *imgBin, int cols, int rows){
 }
 
 
-void changeBit(int *bin){
-    bin[1] = 1;
+void hideImage(unsigned char *secImg, unsigned char *covImg, unsigned char *steImg, int cols, int rows){
+    int secBit, covBit;
+    for(int row = 0; row < rows; row++){
+        for(int col = 0; col < cols; col++){
+            for(int i = 7; i >= 4; i--){
+                secBit = secImg[(row * cols + col) * 8 + (i-4)];
+                covBit = covImg[(row * cols + col) * 8 + i];
+                steImg[(row * cols + col) * 8 + (i-4)] = covBit;
+                steImg[(row * cols + col) * 8 + i] = secBit;
+            }
+        }   
+    }
+}
+
+void getSecImg(unsigned char *steImg, unsigned char *secImg, int cols, int rows){
+    int secBit;
+    for(int row = 0; row < rows; row++){
+        for(int col = 0; col < cols; col++){
+            for(int i = 7; i >= 4; i--){
+                secBit = steImg[(row * cols + col) * 8 + i];
+                secImg[(row * cols + col) * 8 + (i-4)] = secBit;
+            }
+        }   
+    }
 }
 
 int main(int argc, char** argv )
 {
-    unsigned char *secretImgDec, *secretImgBin, *coverImgBin, *secretImgOut;
-    unsigned char matTest[6] = {213,167,130,145,125,118};
-    unsigned char matBin[48];
-    unsigned char matOut[6];
+    unsigned char *secretImgDec, *secretImgBin, *secretImgOut;
+    unsigned char *coverImgDec, *coverImgBin; 
+    unsigned char *stegoImgDec, *stegoImgBin;
+
+    // unsigned char matTest[6] = {213,167,130,145,125,118};
+    // unsigned char matBin[48];
+    // unsigned char matOut[6];
 
     // matTest = (unsigned char*)malloc(sizeof(unsigned char) * 2 * 2);
     // matBin = (unsigned char*)malloc(sizeof(unsigned char) * 2 * 2 * 3 * 8);
     
-    if ( argc != 2 )
+    if ( argc != 3 )
     {
-        printf("usage: DisplaysecretImg.out <secretImg_Path>\n");
+        printf("usage: DisplayImage. <secretImg_Path> <coverImg_Path\n");
         return -1;
     }
 
-    Mat secretImg, coverImg, stegoImg;
-    secretImg = imread( argv[1], 1 );
+    Mat secretImg, coverImg, stegoImg, recovImg;
+
+    secretImg = imread(argv[1], 1);
+    coverImg = imread(argv[2], 1);
+
+    printf("cov > %d x %d\nsec > %d x %d\n",coverImg.rows, coverImg.cols, secretImg.rows, secretImg.cols);
+
     int rows = secretImg.rows;
     int cols = secretImg.cols;
     int colsRGB = cols * secretImg.channels();
@@ -70,6 +100,12 @@ int main(int argc, char** argv )
     secretImgOut = (unsigned char*)malloc(imgSize);
     secretImgBin = (unsigned char*)malloc(imgSizeBin);
 
+    coverImgDec = (unsigned char*)malloc(imgSize);
+    coverImgBin = (unsigned char*)malloc(imgSizeBin);
+
+    stegoImgDec = (unsigned char*)malloc(imgSize);
+    stegoImgBin = (unsigned char*)malloc(imgSizeBin);
+
     if ( !secretImg.data )
     {
         printf("No secretImg data \n");
@@ -77,9 +113,15 @@ int main(int argc, char** argv )
     }
 
     secretImgDec = secretImg.data;
+    coverImgDec = coverImg.data;
 
     imgToBin(secretImgDec, secretImgBin, colsRGB, rows);
-    imgToDec(secretImgBin, secretImgOut, colsRGB, rows);
+    imgToBin(coverImgDec, coverImgBin, colsRGB, rows);
+    hideImage(secretImgBin, coverImgBin, stegoImgBin, colsRGB, rows);
+    imgToDec(stegoImgBin, stegoImgDec, colsRGB, rows);
+    getSecImg(stegoImgBin, secretImgBin, colsRGB, rows);
+    imgToDec(secretImgBin, secretImgDec, colsRGB, rows);
+
     // imgToBin(matTest, matBin, 3, 2);
     // imgToDec(matBin, matOut, 3, 2);
 
@@ -116,12 +158,14 @@ int main(int argc, char** argv )
     //     }   
     // }
     
-    Mat secretImgAns;
-    secretImgAns.create(rows, cols, CV_8UC3);
-    secretImgAns.data = secretImgOut;
+    stegoImg.create(rows, cols, CV_8UC3);
+    stegoImg.data = stegoImgDec;
 
-    imwrite("secretImgOut.jpg", secretImgAns);
-    imwrite("secretImgDec.jpg", secretImg);
+    recovImg.create(rows, cols, CV_8UC3);
+    recovImg.data = secretImgDec;
+
+    imwrite("stegoImgOut.jpg", stegoImg);
+    imwrite("secretImgOut.jpg", secretImg);
 
 
     // namedWindow("Display secretImg", WINDOW_AUTOSIZE );
