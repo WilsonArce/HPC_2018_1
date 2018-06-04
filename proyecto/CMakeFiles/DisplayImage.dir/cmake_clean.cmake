09bb2e826afd4762fa68d45dc7487cd8e5c1924f
WilsonArce <wdarcedev@gmail.com>
2018-06-04 11:42:59 -0500

    cuda image to bin test

--- proyecto/CMakeFiles/DisplayImage.dir/cmake_clean.cmake
+++ proyecto/CMakeFiles/DisplayImage.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/DisplayImage.dir/DisplayImage.cpp.o"
+  "CMakeFiles/DisplayImage.dir/DisplayImage_generated_DisplayImage.cu.o"
   "DisplayImage.pdb"
   "DisplayImage"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/DisplayImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
