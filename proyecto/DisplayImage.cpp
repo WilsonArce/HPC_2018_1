6d6172b468e06a309d0cb406aee8e008676711cd
WilsonArce <wdarcedev@gmail.com>
2018-06-04 10:41:25 -0500

    time test

--- proyecto/DisplayImage.cpp
+++ proyecto/DisplayImage.cpp
@@ -120,7 +120,7 @@ int main(int argc, char** argv )
 
     timeCPU = ((double)(clock() - startCPU))/CLOCKS_PER_SEC;
 
-    printf("%f",timeGPU);
+    printf("%f",timeCPU);
 
     stegoImg.create(rows, cols, CV_8UC3);
     stegoImg.data = stegoImgDec;
